f43a472634b6147fd49c1ea938a0447609aaf6cb
orumpark94 <nowd940@gmail.com>
2025-06-23 17:52:52 +0900

    aws iam role 추aws_iam_role.node_group_role.arn 추가

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -9,3 +9,8 @@ output "cluster_endpoint" {
 output "cluster_ca_certificate" {
   value = aws_eks_cluster.this.certificate_authority[0].data
 }
+
+output "node_role_arn" {
+  description = "IAM Role ARN used by EKS worker nodes"
+  value       = aws_iam_role.node_group_role.arn
+}
