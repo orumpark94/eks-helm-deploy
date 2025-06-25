e0d3c114231216a81f140b2db4e6ebc91a32baeb
orumpark94 <nowd940@gmail.com>
2025-06-25 17:59:21 +0900

    ssm 정책 추가

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -76,6 +76,11 @@ resource "aws_iam_role_policy_attachment" "registry_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 # Node Group 생성
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
